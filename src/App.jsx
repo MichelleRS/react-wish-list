ef9d372b97809637a5d7b64ae99a83b0d1579e3b
Michelle Ross <miross629@gmail.com>
2023-05-11 11:48:49 -0700

    Create static jsx of elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 export default function App() {
   return (
     <>
-      <h1>Hello!</h1>
+      <h1>My Wish List</h1>
+      <h2>Add Item to Wish List</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="item">New Item</label>
+          <input type="text" name="item" id="item" />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      <h2>Wish List Items</h2>
+      <ul>
+        <li>
+          <input type="checkbox" name="purchasedItem1" id="purchasedItem1" />
+          <label htmlFor="purchasedItem1">Item 1</label>
+          <button type="button">Remove</button>
+        </li>
+        <li>
+          <input type="checkbox" name="purchasedItem2" id="purchasedItem2" />
+          <label htmlFor="purchasedItem2">Item 2</label>
+          <button type="button">Remove</button>
+        </li>
+      </ul>
     </>
   );
 }
